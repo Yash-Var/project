965d7197984c827bba10c8eb82f0bcdcd9920708
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-10-30 18:03:41 +0530

    validation

--- starter/controller/tasks.js
+++ starter/controller/tasks.js
@@ -4,8 +4,13 @@ const getAllTask = (req, res) => {
   res.send("completed the setup of controller");
 };
 const postTask = async (req, res) => {
-  const task=await Task.create(req.body)
-  res.status(201).json({task});
+  try {
+    
+    const task=await Task.create(req.body)
+    res.status(201).json({task});
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 };
 const getTask = (req, res) => {
   res.json({id:req.params.id})
--- starter/models/task.js
+++ starter/models/task.js
@@ -1,8 +1,16 @@
 const mongoose=require('mongoose');
 
 const taskSchema=mongoose.Schema({
-    name:String,
-    completed: Boolean,
+    name:{
+        type: String,
+        required:[true,'must provide name'],
+        trim: true,
+        maxlength:[20,'name can not be more than 20 character']
+    },
+    completed: {
+        type:Boolean,
+        default: false
+    },
 });
 
 module.exports=mongoose.model('Task',taskSchema);
