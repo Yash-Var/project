a3358d4496ae52e53fcdfc65b07d50beb0c63ecc
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-10-30 21:02:57 +0530

    completed first project

--- starter/app.js
+++ starter/app.js
@@ -1,28 +1,33 @@
-const express= require('express')
-const app=express();
-const tasks=require("./router/routes")
-const connectDB=require('./DB/connect')
-const port=4040;
-require('dotenv').config()
-// app.use(express.static('./public'))
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+
+app.use(express.static('./public'));
 app.use(express.json());
 
+// routes
 
-app.get('/hello',(req,res)=>{
-    res.send("working fine")
-})
-app.use('/api/v1/tasks',tasks);
+app.use('/api/v1/tasks', tasks);
 
-const start= async ()=>{
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port,()=>{
-            console.log(`server is running on ${port}....`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
-start()
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+start();
