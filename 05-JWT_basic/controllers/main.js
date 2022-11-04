0a16b0cd7fc5f9c227c119c0a6b9fdd1aa2a0b02
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-04 22:49:07 +0530

    jwt

--- 05-JWT_basic/controllers/main.js
+++ 05-JWT_basic/controllers/main.js
@@ -1,14 +1,30 @@
+require('dotenv')
+const jwt=require('jsonwebtoken')
 const CustomAPIError=require('../errors/custom-error')
+
 
 const login=async(req,res)=>{
     const {username,password}=req.body
     if(!username || !password){
         throw new CustomAPIError('Please provide email and password',400)
     }
-    res.send('Fake Login/Register/Signup Route')
+    
+    
+    const id=new Date().getDate()
+// console.log(id,username);
+    const token=jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    res.status(200).json({msg:"user created",token})
 }
 
 const dashboard=async (req,res)=>{
+
+    const authHeader=req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided',401)
+    }
+
+    const token 
+
     const luckyNumber =Math.floor(Math.random()*100)
     res.status(200).json({msg:`yash varshney ${luckyNumber}`,secret:`here is your data`})
 }
