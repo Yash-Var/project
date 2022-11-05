8b929ac3b7f4e4a253357f2577333934feccac10
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-05 15:18:16 +0530

    token genertated succesfully

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -6,7 +6,7 @@ const app = express();
 const connectDB=require('./db/connect')
 const authRouter=require('./routes/auth')
 const jobsRouter=require('./routes/jobs')
-
+const authenticationMiddleware=require('./middleware/authentication')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',authenticationMiddleware,jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 06-jobs-api/starter/middleware/authentication.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const { UnauthenticatedError } = require("../errors");
+
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.userId, name: decoded.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Not authorized to access this route");
+  }
+};
+
+module.exports = authenticationMiddleware;
