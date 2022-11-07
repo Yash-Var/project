e2eaac17781f0ad3f0ecf63ed6422a00816c141b
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-07 21:35:36 +0530

    auth commit

---
+++ Zoom_clone/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app=express();
+const server =require('http').Server(app);
+
+
+app.get('/',(req,res)=>{
+    res.render('room')
+})
+
+
+
+
+
+
+
+
+
+server.listen(3030);
