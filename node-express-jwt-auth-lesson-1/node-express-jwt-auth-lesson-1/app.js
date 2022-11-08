4ad0f8da4c4ef9e30fcacae5e9584145b50794ae
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-08 18:54:30 +0530

    contact form data save

--- node-express-jwt-auth-lesson-1/node-express-jwt-auth-lesson-1/app.js
+++ node-express-jwt-auth-lesson-1/node-express-jwt-auth-lesson-1/app.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
-
+const bodyParser=require('body-parser')
 const app = express();
+const data=require('./models/contactform');
+app.use(bodyParser.urlencoded({extended:true}))
 
 // middleware
 app.use(express.static('public'));
@@ -15,7 +17,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://@cluster0.jqzobx2.mongodb.net/)JWT_AUTH?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://varshney:Sj55888@cluster0.jqzobx2.mongodb.net/)JWT_AUTH?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(9000,()=>{
     console.log('running on port 9000');
@@ -26,4 +28,20 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.post('/',async (req,res)=>{
+try {
+let newdata=new data({
+  name:req.body.name,
+  Mobile:req.body.number,
+  email:req.body.email,
+  date:req.body.date
+});
+newdata.save();
+res.redirect('/');
+  console.log(req.body);
+  
+} catch (error) {
+  console.log(error);
+}
+})
 app.use(authRoutes);
