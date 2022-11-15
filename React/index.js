f0bf274971de779745160ee2471cbcc928b581cd
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-11-15 22:37:23 +0530

    react

--- React/index.js
+++ React/index.js
@@ -1,1 +1,21 @@
-ReactDOM.render(<h1>Hello Everyone! </h1>,document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+const navbar=(
+    <nav>
+        <h1>we care</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+
+ReactDOM.render(
+    navbar
+,
+  document.getElementById("root")
+);
