88c5e63820ade6da42230162275f7167e805542b
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-12-28 20:54:04 +0530

    done

---
+++ React/like-button.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function LikeButton(props) {
+  const [liked, setLiked] = React.useState(false);
+
+  if (liked) {
+    return 'You liked comment number ' + props.commentID;
+  }
+
+  return React.createElement(
+    'button',
+    { onClick: () => setLiked(true) },
+    'Like'
+  );
+}
+
+// Find all DOM containers, and render Like buttons into them.
+// Note we're using CSS classes instead of IDs so that we can find several nodes.
+document.querySelectorAll('.like-button-root').forEach((rootNode) => {
+  const root = ReactDOM.createRoot(rootNode);
+  // Read the comment ID from a data-* attribute.
+  const commentID = parseInt(rootNode.id, 10);
+  root.render(
+    React.createElement(LikeButton, { commentID: commentID })
+  );
+});
