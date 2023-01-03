1743156e9f7b0d8dba2452769a4c5126244b1c12
Yash Varshney <yash.2125cs1155@kiet.edu>
2023-01-03 22:48:07 +0530

    added the k3

--- Email_Scheduler/app.js
+++ Email_Scheduler/app.js
@@ -3,13 +3,19 @@ const { config } = require('dotenv');
 const express=require('express');
 
 const app=express();
-
+const {transporter,options}=require('./email');
 const Scheduler=require('node-cron');
 
 const port =1338;
 
 Scheduler.schedule("* * * * * *",()=>{
     console.log('Inside scheduler');
+    transporter.sendMail(options,(err,info)=>{
+        if(err) {
+            console.error(err);
+        }
+        console.log('Email send with info=',info)
+    })
 });
 
 app.listen(port,()=>{
--- Email_Scheduler/email.js
+++ Email_Scheduler/email.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const nodemailer=require('nodemailer');
 
-const SMTP_PORT=587;
+const SMTP_PORT = 587;
 
-const HOST_SERVICE='smtp-relay.sendinblue.com';
+const HOST_SERVICE='smtp.gmail.com';
 
 const SENDER_EMAIL=process.env.USER_EMAIL;
 
@@ -18,4 +18,23 @@ const EMAIL_SUBJECT="Happy Birthday";
 
 const EMAIL_BODY_HTML="<h1>HAPPY BIRTHDAY </h1>";
 
+const options={
+    from: SENDER_EMAIL,
+    to: RECEIVER_EMAIL,
+    cc: CC,
+    bb: BCC,
+    subject: EMAIL_SUBJECT,
+    html: EMAIL_BODY_HTML
+}
 
+const transporter= nodemailer.createTransport({
+    host: HOST_SERVICE,
+    port: SMTP_PORT,
+    secure: false,
+    auth: {
+        user: SENDER_EMAIL,
+        pass: process.env.USER_PASSWORD,
+    },
+})
+
+module.exports={transporter,options};
